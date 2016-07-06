30cba6e1db8cbd24d47045a88f58e312ad81d960
Conrad Parker <conrad@metadecks.org>
2016-07-07 05:50:56 +1000

    Remove dead code

--- src/Update.elm
+++ src/Update.elm
@@ -56,12 +56,6 @@ update msg model =
                                 , person = step terrain inputs model.person
                                 , inputs = clearStationaryInputs inputs
                         }
-{-
-                          |> walk (directions model.keys)
-                          |> jump model.keys.space
-                          |> gravity (dt / 500)
-                          |> physics (dt / 500)
--}
             in ( model', Cmd.none )
 
 timeToInputs : Time -> Model.Inputs -> Model.Inputs
@@ -81,74 +75,6 @@ mouseToInputs (mx,my) inputs0 = { inputs0 | mx = toFloat mx / 500, my = toFloat 
 
 clearStationaryInputs : Model.Inputs -> Model.Inputs
 clearStationaryInputs inputs0 = { inputs0 | mx = 0, my = 0 }
-
-{-
-directions : Model.Keys -> { x : Int, y : Int }
-directions { left, right, up, down } =
-    let dir a b = case (a,b) of
-            (True, False) -> -1
-            (False, True) -> 1
-            _             -> 0
-    in { x = dir left right, y = dir down up }
-
-flatten : Vec3 -> Vec3
-flatten v =
-    let r = toRecord v
-    in  normalize (vec3 r.x 0 r.z)
-
-turn : Model.MouseMovement -> Model.Person -> Model.Person
-turn (dx,dy) person =
-    let yo x = toFloat (clamp -10 10 x) / 500
-        (roll, pitch, yaw) = Orientation.toRollPitchYaw person.orientation
-        yaw' = yaw - yo dx
-        pitch' = pitch - yo dy
-        pitch'' = clamp (degrees -45) (degrees 45) pitch'
-        orientation = Orientation.fromRollPitchYaw (0, pitch'', yaw')
-    in
-        { person | orientation = orientation }
-
-walk : { x:Int, y:Int } -> Model.Person -> Model.Person
-walk directions person =
---   if getY person.position > Model.eyeLevel then person else
-    let moveDir = normalize (flatten (Model.direction person))
-        strafeDir = transform (makeRotate (degrees -90) j) moveDir
-
-        move = V3.scale (toFloat directions.y) moveDir
-        strafe = V3.scale (toFloat directions.x) strafeDir
-    in
-        { person | velocity = adjustVelocity (move `add` strafe) }
-
-adjustVelocity : Vec3 -> Vec3
-adjustVelocity v =
-    case toTuple v of
-      (0,0,0) -> v
-      _       -> V3.scale 2 (normalize v)
-
-jump : Bool -> Model.Person -> Model.Person
-jump isJumping person =
-  if not isJumping || getY person.position > Model.eyeLevel then person else
-    let v = toRecord person.velocity
-    in
-        { person | velocity = vec3 v.x 2 v.z }
-
-physics : Float -> Model.Person -> Model.Person
-physics dt person =
-    let position = person.position `add` V3.scale dt person.velocity
-        p = toRecord position
-
-        position' = if p.y < Model.eyeLevel
-                    then vec3 p.x Model.eyeLevel p.z
-                    else position
-    in
-        { person | position = position' }
-
-gravity : Float -> Model.Person -> Model.Person
-gravity dt person =
-  if getY person.position <= Model.eyeLevel then person else
-    let v = toRecord person.velocity
-    in
-        { person | velocity = vec3 v.x (v.y - 2 * dt) v.z }
--}
 
 {-
 import Math.Vector3 exposing (..)
