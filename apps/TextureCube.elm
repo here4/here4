eb45ce523a2df6ac033b75aae103b2efd2bd6c65
Conrad Parker <conrad@metadecks.org>
2017-05-10 07:46:34 +0800

    Use dreambuggy to drive texturecube

--- apps/TextureCube.elm
+++ apps/TextureCube.elm
@@ -1,18 +1,24 @@
 module TextureCube exposing (create)
 
-import Math.Vector3 exposing (vec3)
+import Math.Vector3 exposing (Vec3, vec3)
 import Task exposing (Task)
 import Time exposing (Time)
+import Tuple exposing (first)
 import WebGL.Texture as Texture exposing (Texture, Error)
+
+import Math.Quaternion as Qn
+import Orientation exposing (rotateLabV)
 
 import App exposing (App, AppMsg, Focus, appToFocus)
 import Body exposing (..)
 import Control exposing (CtrlMsg)
 import Dispatch exposing (..)
+import Model exposing (Inputs)
 
 import Body.Cube exposing (textureCube)
+import Vehicles.DreamBuggy exposing (dreamBuggy)
 
-type alias TextureCube = List Body
+type alias Model = Maybe (Body, Vec3)
 
 type Msg
     = TextureLoaded (Result Error Texture)
@@ -27,35 +33,54 @@ create label path = App.create (init path)
     , focus = focus
     }
 
-init : String -> (TextureCube, Cmd (CtrlMsg Msg))
+init : String -> (Model, Cmd (CtrlMsg Msg))
 init path =
-    ( []
+    ( Nothing
     , Texture.load path
         |> Task.attempt (Self << TextureLoaded)
     )
 
-update : CtrlMsg Msg -> TextureCube
-    -> (TextureCube, Cmd (CtrlMsg Msg))
+update : CtrlMsg Msg -> Model
+    -> (Model, Cmd (CtrlMsg Msg))
 update msg model = case msg of
     Self (TextureLoaded textureResult) ->
         case textureResult of
-          Ok texture ->
-            ( [ put (vec3 -2 20 -17) (textureCube texture) ] , Cmd.none )
-          Err msg ->
-            -- ( { model | message = "Error loading texture" }, Cmd.none )
-            ( model, Cmd.none )
-    Ctrl (Control.Move dp) -> ( List.map (translate dp) model, Cmd.none )
-    Effect _ -> (model, Cmd.none)
+            Ok texture ->
+                ( Just (put (vec3 -2 20 -17) (textureCube texture), vec3 0 0 0) , Cmd.none )
+            Err msg ->
+                -- ( { model | message = "Error loading texture" }, Cmd.none )
+                ( model, Cmd.none )
 
-animate : Time -> TextureCube -> TextureCube
-animate dt cube = cube
+    Ctrl (Control.Move dp) -> case model of
+        Just (body, vel) -> (Just (translate dp body, vel), Cmd.none)
+        Nothing          -> (Nothing, Cmd.none)
 
-bodies : TextureCube -> List Body
-bodies = identity
+    Ctrl (Control.Drive inputs) ->
+        (drive inputs model, Cmd.none)
 
-camera : TextureCube -> Maybe Camera
-camera cube = Maybe.map bodyCamera (List.head cube)
+    Effect _ ->
+        (model, Cmd.none)
 
-focus : TextureCube -> Maybe Focus
-focus cube = Maybe.map appToFocus (List.head cube)
+animate : Time -> Model -> Model
+animate dt model = model
+
+bodies : Model -> List Body
+bodies model = case model of
+    Just (body, _) -> [body]
+    Nothing        -> []
+
+camera : Model -> Maybe Camera
+camera model = Maybe.map (bodyCamera << first) model
+
+focus : Model -> Maybe Focus
+focus model = Maybe.map (appToFocus << first) model
     
+drive : Inputs -> Model -> Model
+drive inputs model = case model of
+    Just (BCtr anchor scale p o appear, vel) ->
+        let motion0 = { position = p, orientation = Qn.fromVec3 o, velocity = vel }
+            motion = dreamBuggy.move Nothing (always 1.0) inputs motion0
+        in
+            Just ( BCtr anchor scale motion.position (rotateLabV motion.orientation (vec3 0 0 1)) appear
+                 , motion.velocity)
+    Nothing -> Nothing
--- engine/Control.elm
+++ engine/Control.elm
@@ -7,12 +7,14 @@ import Dispatch exposing (..)
 import Dynamic exposing (Dynamic)
 
 import Ground exposing (Ground)
+import Model exposing (Inputs)
 
 type alias CtrlMsg a  = Dispatch EffectMsg Msg a
 type alias WorldMsg a = DispatchHub Bag.Key EffectMsg Msg Dynamic a
 
 type Msg
     = Move Vec3
+    | Drive Inputs
 
 type EffectMsg
     = UpdateGround Ground
