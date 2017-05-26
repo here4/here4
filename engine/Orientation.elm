543300dca6b55bf31099bdfea13fee02d4991f78
Conrad Parker <conrad@metadecks.org>
2017-05-27 07:47:51 +0800

    Use kfish/quaternion

--- engine/Orientation.elm
+++ engine/Orientation.elm
@@ -2,7 +2,7 @@ module Orientation exposing (..)
 
 import Math.Vector3 exposing (Vec3, vec3)
 import Math.Vector3 as V3
-import Math.Quaternion as Qn
+import Quaternion as Qn
 
 type alias Orientation = Qn.Quaternion
 
@@ -10,17 +10,17 @@ initial : Orientation
 initial = Qn.unit
 
 fromRollPitchYaw : (Float, Float, Float) -> Orientation
-fromRollPitchYaw = Qn.fromEuler
+fromRollPitchYaw (roll, pitch, yaw) = Qn.fromYawPitchRoll (yaw, pitch, roll)
 
 toRollPitchYaw : Orientation -> (Float, Float, Float)
-toRollPitchYaw = Qn.toEuler
+toRollPitchYaw o = let (yaw, pitch, roll) = Qn.toYawPitchRoll o in (roll, pitch, yaw)
 
 followedBy : Orientation -> Orientation -> Orientation
-followedBy = Qn.hamilton
+followedBy = Qn.multiply
 
 rotateBodyV : Orientation -> Vec3 -> Vec3
-rotateBodyV = Qn.vrotate
+rotateBodyV = Qn.rotate
 
 rotateLabV : Orientation -> Vec3 -> Vec3
-rotateLabV o = Qn.vrotate (Qn.negate o) 
+rotateLabV o = Qn.rotate (Qn.negate o)
 
