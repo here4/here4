db0b480b7f87a92d45cc5390b70d7f8cbc210b3c
Conrad Parker <conrad@metadecks.org>
2017-06-05 08:12:41 +0800

    Main: elm-format

--- dreambuggy/Main.elm
+++ dreambuggy/Main.elm
@@ -1,39 +1,34 @@
 module Main exposing (main)
 
 import Math.Vector3 exposing (vec3)
-
 import Model exposing (Args)
-
 import NewWorld
 import Boids
 import Balls
 import Sky
 import Statue exposing (VehicleType(..))
 import TextureCube
-
 import StaticGround
 import Body.Terrain as Terrain
-
 import Body.Cube exposing (skyCube, fireCube, fogMountainsCube, voronoiCube)
 import Body.Diamond exposing (cloudsDiamond, fogMountainsDiamond)
 import Body.Sphere exposing (skySphere, cloudsSphere)
 
-main : Program Args
-        (Model.Model NewWorld.Model)
-        (Model.Msg NewWorld.Msg)
+
+main : Program Args (Model.Model NewWorld.Model) (Model.Msg NewWorld.Msg)
 main =
-  NewWorld.create
-    { apps =
-        [ StaticGround.create Terrain.generate
-        , Sky.create skySphere
-        , TextureCube.create "Wooden crate" "resources/woodCrate.jpg"
-        , Boids.create 100
-        , Balls.create 30
-        , Statue.create Bird 3.0 "Clouds Sphere" (vec3 3 10 5) cloudsSphere
-        , Statue.create Buggy 2.0 "Landscape Diamond" (vec3 0 1.5 0) fogMountainsDiamond
-        , Statue.create Bird 4.0 "Sky Diamond" (vec3 5 1.5 1) cloudsDiamond
-        , Statue.create Buggy 7.0 "Voronoi Cube" (vec3 10 0 10) voronoiCube
-        , Statue.create Bird 3.0 "Sky Cube" (vec3 -10 0 -10) skyCube -- fireCube
-        , Statue.create Buggy 4.0 "Landscape Cube" (vec3 10 1.5 -10) fogMountainsCube
-        ]
-    }
+    NewWorld.create
+        { apps =
+            [ StaticGround.create Terrain.generate
+            , Sky.create skySphere
+            , TextureCube.create "Wooden crate" "resources/woodCrate.jpg"
+            , Boids.create 100
+            , Balls.create 30
+            , Statue.create Bird 3.0 "Clouds Sphere" (vec3 3 10 5) cloudsSphere
+            , Statue.create Buggy 2.0 "Landscape Diamond" (vec3 0 1.5 0) fogMountainsDiamond
+            , Statue.create Bird 4.0 "Sky Diamond" (vec3 5 1.5 1) cloudsDiamond
+            , Statue.create Buggy 7.0 "Voronoi Cube" (vec3 10 0 10) voronoiCube
+            , Statue.create Bird 3.0 "Sky Cube" (vec3 -10 0 -10) skyCube -- fireCube
+            , Statue.create Buggy 4.0 "Landscape Cube" (vec3 10 1.5 -10) fogMountainsCube
+            ]
+        }
