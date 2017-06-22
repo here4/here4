5d608de8b66fad1079dc9c2004167664499d5b11
Conrad Parker <conrad@metadecks.org>
2017-06-23 05:51:55 +0800

    Obj: use Appearance.{perspective, lookAt}

--- apps/Body/Obj.elm
+++ apps/Body/Obj.elm
@@ -3,9 +3,13 @@ module Body.Obj exposing (obj)
 import Math.Vector3 exposing (..)
 import Math.Matrix4 as M4 exposing (..)
 import WebGL exposing (..)
+import WebGL.Settings exposing (cullFace, front)
+import WebGL.Settings.DepthTest as DepthTest
 import Appearance exposing (..)
 import Shaders.TextureFragment exposing (textureFragment)
 import Shaders.WorldVertex exposing (Vertex, worldVertex)
+
+import Shaders.Reflection exposing (reflectionVert, reflectionFrag)
 
 import OBJ
 import OBJ.Types exposing (MeshWith, VertexWithTexture)
@@ -15,7 +19,7 @@ type alias Triple a =
 
 
 obj : MeshWith VertexWithTexture -> WebGL.Texture -> Appearance
-obj mesh0 texture p =
+obj mesh texture p =
     let
         resolution =
             vec3 (toFloat p.windowSize.width) (toFloat p.windowSize.height) 0
@@ -26,66 +30,19 @@ obj mesh0 texture p =
             else
                 0.0
     in
-        [ entity worldVertex
-            textureFragment
+        [ entityWith [ DepthTest.default, cullFace front ]
+            reflectionVert
+            reflectionFrag
             mesh
+            -- { camera = camera, mvMat = modelView, texture = texture }
+            { camera = p.perspective, mvMat = p.lookAt, texture = texture }
+{-
             { iResolution = resolution
             , iHMD = iHMD
             , iTexture = texture
             , iLensDistort = p.lensDistort
             , view = p.viewMatrix
             }
-        ]
-
-
-{-| The mesh for a cube
 -}
-mesh : Mesh Vertex
-mesh =
-    triangles <| List.concatMap rotatedFace [ ( 0, 0, 0 ), ( 90, 0, 1 ), ( 180, 0, 2 ), ( 270, 0, 3 ), ( 0, 90, 0 ), ( 0, -90, 0 ) ]
-
-
-rotatedFace : ( Float, Float, Float ) -> List (Triple Vertex)
-rotatedFace ( angleX, angleY, coordX ) =
-    let
-        x =
-            makeRotate (degrees angleX) (vec3 1 0 0)
-
-        y =
-            makeRotate (degrees angleY) (vec3 0 1 0)
-
-        t =
-            M4.transform (mul x (mul y (makeTranslate (vec3 0 0 0.5))))
-            >> add (vec3 0 0.5 0)
-
-        each f ( a, b, c ) =
-            ( f a, f b, f c )
-    in
-        List.map (each (\x -> { x | pos = t x.pos, coord = add (vec3 coordX 0 0) x.coord })) face
-
-
-face : List (Triple Vertex)
-face =
-    let
-        white =
-            vec3 1 1 1
-
-        topLeft =
-            -- { pos = vec3 -1 1 0, color = white, coord = vec3 0 1 0 }
-            { pos = vec3 -0.5 0.5 0, color = white, coord = vec3 0 1 0 }
-
-        topRight =
-            -- { pos = vec3 1 1 0, color = white, coord = vec3 1 1 0 }
-            { pos = vec3 0.5 0.5 0, color = white, coord = vec3 1 1 0 }
-
-        bottomLeft =
-            -- { pos = vec3 -1 -1 0, color = white, coord = vec3 0 0 0 }
-            { pos = vec3 -0.5 -0.5 0, color = white, coord = vec3 0 0 0 }
-
-        bottomRight =
-            -- { pos = vec3 1 -1 0, color = white, coord = vec3 1 0 0 }
-            { pos = vec3 0.5 -0.5 0, color = white, coord = vec3 1 0 0 }
-    in
-        [ ( topLeft, topRight, bottomLeft )
-        , ( bottomLeft, topRight, bottomRight )
         ]
+
