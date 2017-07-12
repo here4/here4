7c4868aaf7cdf9f3988518214f065bef4c9efb2d
Conrad Parker <conrad@metadecks.org>
2017-07-13 03:25:15 +0800

    Object: new module

---
+++ apps/Object.elm
@@ -0,0 +1,69 @@
+module Object exposing
+    ( ObjectAttributes(..)
+    , ObjectResult(..)
+    , ObjectMsg(..)
+    , objectInit
+    , objectUpdate
+    )
+
+
+import Object.ReflectiveObj exposing (..)
+import Object.TexturedObj exposing (..)
+import Object.Types exposing (Load(..))
+import Tuple
+
+type ObjectAttributes
+    = TexturedObj TexturedObjAttributes
+    | ReflectiveObj ReflectiveObjAttributes
+
+type ObjectResult
+    = TexturedObjResult TexturedObjResult
+    | ReflectiveObjResult ReflectiveObjResult
+
+type ObjectMsg
+    = TexturedObjMsg TexturedObjMsg
+    | ReflectiveObjMsg ReflectiveObjMsg
+
+
+wrap :
+    (result -> ObjectResult)
+    -> (msg -> ObjectMsg)
+    -> (Load result, Cmd msg)
+    -> (Load ObjectResult, Cmd ObjectMsg)
+wrap t m (model, msg) =
+    case model of
+        Loading model_ ->
+            ( Loading (t model_), Cmd.map m msg )
+        Ready appear ->
+            ( Ready appear, Cmd.none )
+
+
+objectInit : ObjectAttributes -> (Load ObjectResult, Cmd ObjectMsg)
+objectInit attributes =
+    case attributes of
+        TexturedObj obj ->
+            texturedObjInit obj
+            |> wrap TexturedObjResult TexturedObjMsg
+        ReflectiveObj obj ->
+            reflectiveObjInit obj
+            |> wrap ReflectiveObjResult ReflectiveObjMsg
+
+
+objectUpdate : ObjectMsg -> Load ObjectResult -> (Load ObjectResult, Cmd ObjectMsg)
+objectUpdate msg model =
+    case model of
+        Ready appear ->
+            ( Ready appear, Cmd.none )
+
+        Loading partial ->
+            case (msg, partial) of
+                (TexturedObjMsg msg_, TexturedObjResult model_) ->
+                    texturedObjUpdate msg_ (Loading model_)
+                    |> wrap TexturedObjResult TexturedObjMsg
+                (ReflectiveObjMsg msg_, ReflectiveObjResult model_) ->
+                    reflectiveObjUpdate msg_ (Loading model_)
+                    |> wrap ReflectiveObjResult ReflectiveObjMsg
+                _ ->
+                    ( model, Cmd.none )
+                
+
