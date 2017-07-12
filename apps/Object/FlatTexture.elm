1e9377de418a4ae3499ba152dbbe130ee9df82b6
Conrad Parker <conrad@metadecks.org>
2017-07-13 04:51:04 +0800

    FlatTexture: new module

---
+++ apps/Object/FlatTexture.elm
@@ -0,0 +1,106 @@
+module Object.FlatTexture exposing
+    ( FlatTextureAttributes
+    , FlatTextureResult
+    , FlatTextureMsg(..)
+    , flatTextureInit
+    , flatTextureUpdate
+    )
+
+
+import Appearance exposing (Appearance)
+import Body.Obj exposing (reflective)
+import Dict exposing (Dict)
+import Math.Vector3 exposing (vec3)
+import Object.Types exposing (Load(..))
+import OBJ
+import OBJ.Types exposing (MeshWith, VertexWithTexture)
+import Shaders.TextureFragment exposing (textureFragment)
+import Shaders.WorldVertex exposing (Vertex, worldVertex)
+import Task exposing (Task)
+import WebGL exposing (entity, Mesh)
+import WebGL.Texture as Texture exposing (Texture, Error)
+
+
+type alias FlatTextureAttributes =
+    { mesh : Mesh Vertex
+    , texturePath : String
+    }
+
+type alias FlatTextureResult =
+    { mesh : Mesh Vertex
+    , texture : Result String Texture
+    }
+
+type FlatTextureMsg
+    = TextureLoaded (Result String Texture)
+
+
+flatTextureInit : FlatTextureAttributes -> (Load FlatTextureResult, Cmd FlatTextureMsg)
+flatTextureInit attributes =
+    ( Loading
+          { mesh = attributes.mesh
+          , texture = Err "Loading texture ..."
+          }
+    -- , Texture.load attributes.texturePath
+    --     |> Task.attempt TextureLoaded
+    , loadTexture attributes.texturePath TextureLoaded
+    )
+
+
+flatTextureUpdate : FlatTextureMsg -> Load FlatTextureResult -> (Load FlatTextureResult, Cmd FlatTextureMsg)
+flatTextureUpdate msg model =
+    let
+        loadBody m =
+            case m.texture of
+                ( Ok texture_ ) ->
+                    Ready (makeAppearance m.mesh texture_)
+                _ ->
+                    Loading m
+    in
+        case model of
+            Ready appear ->
+                ( Ready appear, Cmd.none )
+
+            Loading partial ->
+            
+                case msg of
+                    TextureLoaded textureResult ->
+                        ( loadBody { partial | texture = textureResult }, Cmd.none )
+
+loadTexture : String -> (Result String Texture -> msg) -> Cmd msg
+loadTexture url msg =
+    Texture.load url
+        |> Task.attempt
+            (\r ->
+                case r of
+                    Ok t ->
+                        msg (Ok t)
+
+                    Err e ->
+                        msg (Err ("Failed to load texture: " ++ toString e))
+            )
+
+
+makeAppearance : Mesh Vertex -> Texture -> Appearance
+makeAppearance mesh texture p =
+    let
+        resolution =
+            vec3 (toFloat p.windowSize.width) (toFloat p.windowSize.height) 0
+
+        iHMD =
+            if p.cameraVR then
+                1.0
+            else
+                0.0
+    in
+        [ entity worldVertex
+            textureFragment
+            mesh
+            { iResolution = resolution
+            , iHMD = iHMD
+            , iTexture = texture
+            , iLensDistort = p.lensDistort
+            , iPerspective = p.perspective
+            , iLookAt = p.lookAt
+            }
+        ]
