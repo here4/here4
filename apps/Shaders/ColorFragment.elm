25e12ed58b8578b68513676fa3f332bc47dd6eca
Conrad Parker <conrad@metadecks.org>
2017-07-21 05:38:49 +0800

    ColorFragment: shift rgb separately with distance

--- apps/Shaders/ColorFragment.elm
+++ apps/Shaders/ColorFragment.elm
@@ -128,9 +128,12 @@ void texture(vec2 tc) {
 
 	vec4 fractalTexture = vec4(c * cos(1.57 * gl_FragCoord.y / iResolution.y), elm_FragColor.a);
         vec4 flatTexture = elm_FragColor;
-        //gl_FragColor = mix(fractalTexture, flatTexture, iSmoothing);
-        float lightenDistance = clamp (0.9, 1.0, 1.0 - (float(worldPosition.w)/200.0));
-        gl_FragColor = mix(fractalTexture, flatTexture, iSmoothing) * lightenDistance;
+
+        gl_FragColor = mix(fractalTexture, flatTexture, iSmoothing);
+
+        float lightenDistance = worldPosition.w * 0.01;
+
+        gl_FragColor *= 1.0 - lightenDistance * vec4(0.07, 0.09, 0.10, 0.15);
 }
 
 void hmd() {
