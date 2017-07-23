92343076ef8096f5f2d94210ce4c333bc7f64646
Conrad Parker <conrad@metadecks.org>
2017-07-23 21:55:29 +0800

    TextureFragment: code up hmdTemplate

--- apps/Shaders/TextureFragment.elm
+++ apps/Shaders/TextureFragment.elm
@@ -1,37 +1,45 @@
-module Shaders.TextureFragment exposing (textureFragment)
+module Shaders.TextureFragment exposing (textureFragment, hmdTemplate)
 
+import GLSLPasta
+import GLSLPasta.Types exposing (Global(..), Template)
 import Math.Vector2 exposing (Vec2)
 import Math.Vector3 exposing (Vec3)
 import WebGL exposing (Shader, Texture)
 
 
-textureFragment : Shader {} { u | iResolution : Vec3, iHMD : Float, iTexture : Texture } { elm_FragColor : Vec3, elm_FragCoord : Vec2 }
-textureFragment =
-    [glsl|
-
+hmdTemplate : Template
+hmdTemplate =
+    { id = "hmdTemplate"
+    , globals =
+        [ Uniform "vec3" "iResolution"
+        , Uniform "float" "iHMD"
+        , Uniform "sampler2D" "iTexture"
+        , Varying "vec3" "elm_FragColor"
+        , Varying "vec2" "elm_FragCoord"
+        , Const "vec2" "Scale" "vec2(0.1469278, 0.2350845)"
+        -- , Const "vec2" "Scale" "vec2(0.1469278, 0.2350845)"
+        , Const "vec2" "ScaleIn" "vec2(3, 2.5)"
+        -- , Const "vec2" "ScaleIn" "vec2(2.5, 1.5)"
+        , Const "vec4" "HmdWarpParam" "vec4(1, 0.22, 0.24, 0)"
+        ]
+    , template = """
 precision mediump float;
-uniform vec3 iResolution;
-uniform float iHMD;
-uniform sampler2D iTexture;
 
-varying vec3 elm_FragColor;
-varying vec2 elm_FragCoord;
+__PASTA_GLOBALS__
 
-const vec2 Scale = vec2(0.1469278, 0.2350845);
-//const vec2 Scale = vec2(0.1469278, 0.2350845);
-const vec2 ScaleIn = vec2(3, 2.5);
-//const vec2 ScaleIn = vec2(2.5, 1.5);
-const vec4 HmdWarpParam   = vec4(1, 0.22, 0.24, 0);
+__PASTA_FUNCTIONS__
+
+__PASTA_SPLICES__
 
 // Scales input texture coordinates for distortion.
 vec2 HmdWarp(vec2 in01, vec2 LensCenter)
 {
-	vec2 theta = (in01 - LensCenter) * ScaleIn; // Scales to [-1, 1]
-	float rSq = theta.x * theta.x + theta.y * theta.y;
-	vec2 rvector = theta * (HmdWarpParam.x + HmdWarpParam.y * rSq +
-		HmdWarpParam.z * rSq * rSq +
-		HmdWarpParam.w * rSq * rSq * rSq);
-	return LensCenter + Scale * rvector;
+    vec2 theta = (in01 - LensCenter) * ScaleIn; // Scales to [-1, 1]
+    float rSq = theta.x * theta.x + theta.y * theta.y;
+    vec2 rvector = theta * (HmdWarpParam.x + HmdWarpParam.y * rSq +
+        HmdWarpParam.z * rSq * rSq +
+        HmdWarpParam.w * rSq * rSq * rSq);
+    return LensCenter + Scale * rvector;
 }
 
 void hmd() {
@@ -56,5 +64,10 @@ void main() {
     else
         gl_FragColor = texture2D(iTexture, elm_FragCoord);
 }
+"""
+    }
 
-|]
+textureFragment : Shader {} { u | iResolution : Vec3, iHMD : Float, iTexture : Texture } { elm_FragColor : Vec3, elm_FragCoord : Vec2 }
+textureFragment =
+    GLSLPasta.combineUsingTemplate hmdTemplate []
+    |> WebGL.unsafeShader
