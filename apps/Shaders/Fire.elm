c55267a9c953b486b3a35b86e545966fc2e79b34
Conrad Parker <conrad@metadecks.org>
2017-07-25 04:47:56 +0800

    Fire: use GLSLPasta

--- apps/Shaders/Fire.elm
+++ apps/Shaders/Fire.elm
@@ -1,102 +1,84 @@
 module Shaders.Fire exposing (fire)
 
+import GLSLPasta
+import GLSLPasta.Core exposing (empty)
+import GLSLPasta.Lighting as Lighting
+import GLSLPasta.Types as GLSLPasta exposing (Global(..))
 import Math.Vector2 exposing (Vec2)
 import Math.Vector3 exposing (..)
 import Math.Vector4 exposing (Vec4)
+import Shaders.HMD exposing (hmdTemplate)
 import WebGL exposing (..)
 
 
 -- https://www.shadertoy.com/view/Xsl3zN
 
-
-fire : Shader {} { u | iResolution : Vec3, iGlobalTime : Float, iHMD : Float } { elm_FragColor : Vec4, elm_FragCoord : Vec2 }
-fire =
-    [glsl|
-
-precision mediump float;
-uniform vec3 iResolution;
-uniform float iGlobalTime;
-uniform float iHMD;
-
-varying vec4 elm_FragColor;
-varying vec2 elm_FragCoord;
-
-const vec2 Scale = vec2(0.1469278, 0.2350845);
-//const vec2 Scale = vec2(0.1469278, 0.2350845);
-const vec2 ScaleIn = vec2(3, 2.5);
-//const vec2 ScaleIn = vec2(2.5, 1.5);
-const vec4 HmdWarpParam   = vec4(1, 0.22, 0.24, 0);
-
-// Scales input texture coordinates for distortion.
-vec2 HmdWarp(vec2 in01, vec2 LensCenter)
-{
-	vec2 theta = (in01 - LensCenter) * ScaleIn; // Scales to [-1, 1]
-	float rSq = theta.x * theta.x + theta.y * theta.y;
-	vec2 rvector = theta * (HmdWarpParam.x + HmdWarpParam.y * rSq +
-		HmdWarpParam.z * rSq * rSq +
-		HmdWarpParam.w * rSq * rSq * rSq);
-	return LensCenter + Scale * rvector;
-}
-
+fragment_fire : GLSLPasta.Component
+fragment_fire =
+    { empty
+        | id = "fragment_fire"
+        , provides =
+            [ "gl_FragColor"
+            ]
+        , globals =
+            [ Uniform "vec3" "iResolution"
+            , Uniform "float" "iGlobalTime"
+            , Uniform "float" "iDetail"
+            , Varying "vec4" "elm_FragColor"
+            , Varying "vec2" "elm_FragCoord"
+            ]
+        , functions =
+            [ """
 // by @301z
 
 float rand(vec2 n) {
-	return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+        return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
 }
 
 float noise(vec2 n) {
-	const vec2 d = vec2(0.0, 1.0);
-	vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
-	return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
+        const vec2 d = vec2(0.0, 1.0);
+        vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+        return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
 }
 
 float fbm(vec2 n) {
-	float total = 0.0, amplitude = 1.0;
-	for (int i = 0; i < 7; i++) {
-		total += noise(n) * amplitude;
-		n += n;
-		amplitude *= 0.5;
-	}
-	return total;
+        float total = 0.0, amplitude = 1.0;
+        for (int i = 0; i < 7; i++) {
+                total += noise(n) * amplitude;
+                n += n;
+                amplitude *= 0.5;
+        }
+        return total;
 }
 
-void fire(vec2 tc)
+vec4 fire(vec2 tc)
 {
-	const vec3 c1 = vec3(0.1, 0.0, 0.0);
-	const vec3 c2 = vec3(0.7, 0.0, 0.0);
-	const vec3 c3 = vec3(0.2, 0.0, 0.0);
-	const vec3 c4 = vec3(1.0, 0.9, 0.0);
-	const vec3 c5 = vec3(0.1);
-	const vec3 c6 = vec3(0.9);
-	//vec2 p = gl_FragCoord.xy * 8.0 / iResolution.xx;
-	vec2 p = tc.xy * 8.0;
-	float q = fbm(p - iGlobalTime * 0.1);
-	vec2 r = vec2(fbm(p + q + iGlobalTime * 0.7 - p.x - p.y), fbm(p + q - iGlobalTime * 0.4));
-	vec3 c = mix(c1, c2, fbm(p + r)) + mix(c3, c4, r.x) - mix(c5, c6, r.y);
-	gl_FragColor = vec4(c * cos(1.57 * gl_FragCoord.y / iResolution.y), 1.0);
+        const vec3 c1 = vec3(0.1, 0.0, 0.0);
+        const vec3 c2 = vec3(0.7, 0.0, 0.0);
+        const vec3 c3 = vec3(0.2, 0.0, 0.0);
+        const vec3 c4 = vec3(1.0, 0.9, 0.0);
+        const vec3 c5 = vec3(0.1);
+        const vec3 c6 = vec3(0.9);
+        //vec2 p = gl_FragCoord.xy * 8.0 / iResolution.xx;
+        vec2 p = tc.xy * 8.0;
+        float q = fbm(p - iGlobalTime * 0.1);
+        vec2 r = vec2(fbm(p + q + iGlobalTime * 0.7 - p.x - p.y), fbm(p + q - iGlobalTime * 0.4));
+        vec3 c = mix(c1, c2, fbm(p + r)) + mix(c3, c4, r.x) - mix(c5, c6, r.y);
+        return vec4(c * cos(1.57 * gl_FragCoord.y / iResolution.y), 1.0);
 }
-
-void hmd() {
-    vec2 LensCenter = vec2(0.5, 0.5);
-    vec2 ScreenCenter = vec2(0.5, 0.5);
-
-    vec2 oTexCoord = gl_FragCoord.xy / iResolution.xy;
-
-    vec2 tc = HmdWarp(oTexCoord, LensCenter);
-    if (any(bvec2(clamp(tc,ScreenCenter-vec2(0.5,0.5), ScreenCenter+vec2(0.5,0.5)) - tc)))
-    {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-        return;
+"""
+            ]
+        , splices =
+            [ """
+        gl_FragColor = fire(fragCoord);
+                """
+            ]
     }
 
-    fire(tc);
-}
-
-void main() {
-    if (iHMD == 1.0)
-        hmd();
-    else
-        fire(elm_FragCoord);
-}
-
-|]
+fire : Shader {} { u | iResolution : Vec3, iGlobalTime : Float, iHMD : Float } { elm_FragColor : Vec4, elm_FragCoord : Vec2 }
+fire =
+    GLSLPasta.combineUsingTemplate hmdTemplate
+        [ fragment_fire
+        , Lighting.lightenDistance
+        ]
+    |> WebGL.unsafeShader
