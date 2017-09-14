3675926120c7116fc008e5d9c44bd03d53c01b20
Conrad Parker <conrad@metadecks.org>
2017-09-14 17:59:17 +0800

    Road: update roadSides

--- city/Road.elm
+++ city/Road.elm
@@ -1,14 +1,13 @@
-module Road
-    exposing
-        ()
+module Road exposing (..)
 
-
+import Geometry.VertexStrip exposing (..)
 import Here4.App as App exposing (..)
 import Here4.App.Types exposing (..)
 import Here4.Body exposing (..)
 import Html exposing (Html)
 import Html.Attributes as Html
 import Math.Vector3 as V3 exposing (Vec3, vec3)
+import WebGL exposing (Mesh, triangleStrip)
 
 
 type alias Model =
@@ -38,13 +37,18 @@ create path =
 init : List Vec3 -> ( Model, Cmd (CtrlMsg Msg) )
 init path =
     let
-        bodies = generateRoad path
+        bodies = generateRoad 5.0 path
     in
         ( { path = path
           , bodies = bodies
           }
         , Cmd.none
         )
+
+
+update : CtrlMsg Msg -> Model -> ( Model, Cmd (CtrlMsg Msg) )
+update msg model =
+    ( model, Cmd.none )
 
 
 animate : Ground -> Time -> Model -> ( Model, Cmd (CtrlMsg Msg) )
@@ -72,14 +76,23 @@ type alias RoadVertex =
     }
 
 
-generateRoad : List Vec3 -> List Body
-generateRoad path =
+generateRoad : Float -> List Vec3 -> List Body
+generateRoad sideWidth path =
+    -- entity etc.
     []
 
 
-roadMesh : List Vec3 -> List (Mesh RoadVertex)
-roadMesh path =
-    []
+roadMesh : Float -> List Vec3 -> List (Mesh RoadVertex)
+roadMesh sideWidth path =
+    let
+        (leftSide, rightSide) =
+            roadSides sideWidth path
+    in
+        [ triangleStrip (mkStrip leftSide rightSide) ]
+    
+
+-- roadSides : Float -> List Vec3 -> (List RoadVertex, List RoadVertex)
+-- roadSides sideWidth path =
 
 
 roadSides : Float -> List Vec3 -> (List Vec3, List Vec3)
@@ -120,8 +133,38 @@ corner sideWidth v1 v2 v3 =
     let
         offset12 = sideOffset sideWidth v1 v2
         offset23 = sideOffset sideWidth v2 v3
+
+        -- Just find where the second line intersects the first plane
+
+        p1 = V3.add v1 offset12
+        p2 = V3.add v2 offset23
+
+        norm u v w =
+            V3.normalize <| V3.cross (V3.sub u v) (V3.sub w v)
+
+        norm12 = norm p1 v1 v2
+        norm23 = norm p2 v2 v3
+
+        -- V3.cross norm12 norm23
+
+        getXZ v = (V3.getX v, V3.getZ v)
+
+        -- The two roadsides, in (x,y) notation
+        (x1, y1) = getXZ <| V3.add v1 offset12
+        (x2, y2) = getXZ <| V3.add v2 offset12
+        (x3, y3) = getXZ <| V3.add v2 offset23
+        (x4, y4) = getXZ <| V3.add v3 offset23
+
+        denominator = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+
+        x = (x1*y2 - y1*x2)*(x3-x4) - (x1-x2)*(x3*y4 - y3*x4)
+        y = (x1*y2 - y1*x2)*(y3-y4) - (y1-y2)*(x3*y4 - y3*x4)
     in
-        V3.add v2 (V3.scale 0.5 (V3.add offset12 offset23))
+        if abs denominator < 0.0001 then
+            V3.add v2 offset12
+        else
+            vec3 (x/denominator) 0 (y/denominator)
+            
 
 
 -- Generate the corner vertices for a roadside, and the end
@@ -129,7 +172,7 @@ sideCorners : Float -> List Vec3 -> List Vec3
 sideCorners sideWidth path =
     case path of
         ( v1 :: v2 :: v3 :: rest ) ->
-            corner sideWidth v1 v2 v3 :: sideCorners (v2 :: v3 :: rest)
+            corner sideWidth v1 v2 v3 :: sideCorners sideWidth (v2 :: v3 :: rest)
         [v1, v2] ->
             [v2]
         _ ->
